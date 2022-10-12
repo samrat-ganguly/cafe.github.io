b480ed4c6a745c3d9f7bdf98185f73e8c14cf5f4
Samrat Ganguly <81913029+samrat-ganguly@users.noreply.github.com>
2022-10-13 04:11:37 +0530

    Add files via upload

---
+++ cafe.js
@@ -0,0 +1,69 @@
+// header logo
+ScrollReveal().reveal(".logo", {
+  duration: 1800,
+  delay: 0,
+  distance: "40px",
+  origin: "top",
+});
+
+//hero tag
+ScrollReveal().reveal(".a1", {
+  duration: 1800,
+  delay: 400,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a2", {
+  duration: 1800,
+  delay: 800,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a3", {
+  duration: 1800,
+  delay: 1200,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a4", {
+  duration: 1800,
+  delay: 1600,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a5", {
+  duration: 1800,
+  delay: 2000,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a6", {
+  duration: 1800,
+  delay: 2400,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a7", {
+  duration: 1800,
+  delay: 2800,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a8", {
+  duration: 1800,
+  delay: 3100,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a9", {
+  duration: 1800,
+  delay: 3400,
+  distance: "40px",
+  origin: "left",
+});
+ScrollReveal().reveal(".a10", {
+  duration: 1800,
+  delay: 3700,
+  distance: "40px",
+  origin: "left",
+});
